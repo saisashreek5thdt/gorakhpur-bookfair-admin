dd4213ff123ab4a45ea03552298bf53404ac7c0b
Sai Sashreek D <saisashreekd@gmail.com>
2025-01-29 14:18:45 +0530

    Content Updated

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-teal-600 font-medium">
+        Welcome To Gorakhpur Book Fair
+      </h1>
+    </>
+  );
+}
+
+export default App;
