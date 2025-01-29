16c38d138e6fd7c6d5e07c7da27e4b710b8841bd
Sai Sashreek D <saisashreekd@gmail.com>
2025-01-29 15:56:25 +0530

    New Dashboard Page Created

---
+++ src/dashbaord/Dashboard.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 transition-[grid-template-columns] lg:grid-cols-[120px_1fr] lg:gap-8 lg:[&:has(>*:first-child:hover)]:grid-cols-[160px_1fr]">
+        <div className="h-32 rounded-lg bg-gray-200"></div>
+        <div className="h-32 rounded-lg bg-gray-200"></div>
+      </div>
+    </>
+  );
+}
