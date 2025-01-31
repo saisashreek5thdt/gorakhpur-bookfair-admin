1512e6368938b570b801abcae70bb2028030e9d3
Vishal Payyawar <vishal.payyawar22@vit.edu>
2025-01-31 16:14:43 +0530

    logout butoon fix

--- src/Componants/Sidebar/SidebarLogout.jsx
+++ src/Componants/Sidebar/SidebarLogout.jsx
@@ -1,12 +1,21 @@
 import { FiLogOut } from "react-icons/fi";
 
 export default function SidebarLogout() {
+  // Handle Logout Functionality
+  const handleLogout = () => {
+    // Add your logout logic here
+    console.log("User logged out");
+  };
+
   return (
-    <div className="mt-auto p-4 border-t border-gray-200 text-lg select-none">
-      <div className="flex items-center cursor-pointer hover:bg-gray-100 p-2 rounded-lg transition-colors duration-200">
+    <div className="fixed bottom-0 left-0 w-64 bg-white  border-gray-200 p-4 select-none">
+      <div
+        className="flex items-center cursor-pointer hover:bg-gray-100 p-2 rounded-lg transition-colors duration-200"
+        onClick={handleLogout}
+      >
         {/* Logout Icon */}
-        <FiLogOut />
-        <span className="ml-2">Logout</span>
+        <FiLogOut className="text-lg cursor-pointer" />
+        <span className="ml-2 text-md font-medium cursor-pointer">Logout</span>
       </div>
     </div>
   );
